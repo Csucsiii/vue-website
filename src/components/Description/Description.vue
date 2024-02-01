a62189f9033ca484cff232f22148aefeffeb59d9
Csucsi <csucsi.eth@protonmail.com>
2024-02-01 04:45:51 +0100

    Added description

--- src/components/Description/Description.vue
+++ src/components/Description/Description.vue
@@ -1,19 +1,19 @@
 <template>
-	<div className="flex flex-col items-center w-[100%] text-slate-200 gap-2">
+	<div class="flex flex-col items-center w-[100%] text-slate-200 gap-2">
 		<v-title label="Szerverünk" #icon>
 			<v-icon name="fa-info-circle" scale="2.5" />
 		</v-title>
-		<div className="flex flex-col-reverse lg:flex-row items-center w-[90%] lg:w-[70%] gap-2 h-full">
-			<div className="flex justify-center h-[100%] w-[80%] md:w-[50%] lg:w-[40%] xl:w-[30%]">
-				<div className="flex justify-center items-center w-[70%] aspect-square -z-[9]">
-					<img loading="lazy" className="bg" src="/astronaut.webp" alt="astronaut_img" />
+		<div class="flex flex-col-reverse lg:flex-row items-center w-[90%] lg:w-[70%] gap-2 h-full">
+			<div class="flex justify-center h-[100%] w-[80%] md:w-[50%] lg:w-[40%] xl:w-[30%]">
+				<div class="flex justify-center items-center w-[70%] aspect-square -z-[9]">
+					<img loading="lazy" class="bg" src="/astronaut.webp" alt="astronaut_img" />
 				</div>
 			</div>
 
 			<div
-				className="flex flex-col justify-center items-center text-slate-200 w-[100%] lg:w-[70%] h-[100%] text-center text-sm lg:text-lg left-[5%] bg-zinc-800/60 p-2 rounded-md"
+				class="flex flex-col justify-center items-center text-slate-200 w-[100%] lg:w-[70%] h-[100%] text-center text-sm lg:text-lg left-[5%] bg-zinc-800/60 p-2 rounded-md"
 			>
-				<div className="m-2">
+				<div class="m-2">
 					A
 					<v-link href="https://discord.gg/galaxyrp" ariaLabel="Galaxy Roleplay hivatalos oldala." #name> Galaxy Roleplay </v-link>
 					egy GTA V szerepjáték szerver, amelynek tulajdonosai
@@ -26,7 +26,7 @@
 					<v-link href="https://fivem.net" ariaLabel="FiveM hivatalos oldala" #name> FiveM </v-link>
 					. A szerveren a fejlesztők által készített egyéni szkriptek találhatók, amelyekhez rendszeresen érkeznek frissítések.
 				</div>
-				<div className="m-2">
+				<div class="m-2">
 					A szerepjáték lényege, hogy beszélj, cselekedj, és úgy járj el, mint a karaktered, akit alakítasz. Ez azt jelenti, hogy a játék közben végrehajtott
 					minden művelet a karaktered nevében történik. A cél egy olyan magával ragadó élmény megteremtése, ahol a játékosok és a nézők egyaránt átélhetnek
 					történeteket és helyzeteket, egy lényegében egy másik világban.
@@ -54,3 +54,11 @@
 		},
 	});
 </script>
+
+<style lang="scss" scoped>
+	.bg {
+		transition: all 350ms ease-in-out;
+		animation: floatAstronaut 5s linear infinite;
+		overflow: hidden;
+	}
+</style>
